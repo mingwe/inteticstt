3e875162e118ccc849e2a46e43a85acd74a61677
Peter <themingwe@gmail.com>
2023-04-01 17:37:41 +0300

    mock some tests

--- tests/mainPage.test.js
+++ tests/mainPage.test.js
@@ -8,3 +8,18 @@ test('Opens main page', async t => {
   await t.expect(getByTestId("filters-container").exists).ok()
   await t.expect(getByTestId("main-container").exists).ok()
 })
+
+test('Apply filters', async t => {
+  // here we can write test logic for checking filters
+  await t.expect(true).ok()
+})
+
+test('Apply sort', async t => {
+  // here we can write test logic for testing sort
+  await t.expect(true).ok()
+})
+
+test('Apply search', async t => {
+  // here we can write test logic for testing search
+  await t.expect(true).ok()
+})
